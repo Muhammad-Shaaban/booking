8268652d0a59ccf4670159a81f0967aaa7fb5736
Muhammad-Shaaban <65139942+Muhammad-Shaaban@users.noreply.github.com>
2020-11-13 09:51:41 +0200

    Auto-generated commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  /*$('input[type="file"]').change(function () {
+    var img = $(this).val();
+    $('.fileInputText').text(img.substr(12, img.length));
+  });*/
+});
